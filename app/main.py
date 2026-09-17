06f0db757f769be68eea1d1eda0d0459157dd272
Diya Baheti <diyapbaheti@gmail.com>
2026-09-17 10:09:49 +0530

    added basic healthcheck endpoint

--- app/main.py
+++ app/main.py
@@ -13,3 +13,18 @@ async def root():
     return{
         "message":"Welcome to Crypto API. Go to /docs for Swagger documentation"
     }
+
+@app.get("/health", tags=["System"])
+async def health_check():
+    """
+    check health status of application and external services 
+    """
+    #make external service dynamic after connecting to the CoinGecko API
+    return{
+        "app_status":"healthy",
+        "app_version":"1.0.0",
+        "external_service":{
+            "status":"reachable",
+            "version":"v3" 
+        }
+    }
