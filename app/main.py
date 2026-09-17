241bddad7949ca930e74ece25b2625039e8b6a0f
Diya Baheti <diyapbaheti@gmail.com>
2026-09-17 11:06:45 +0530

    added list coins endpoint with pagination

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-from app.services.coingecko import ping_coingecko
+from fastapi import FastAPI, Query
+from app.services.coingecko import ping_coingecko, get_all_coins 
 
 #initialize fastAPI application
 app = FastAPI(
@@ -27,3 +27,26 @@ async def health_check():
         "app_version":"1.0.0",
         "external_service": external_status
     }
+
+@app.get("/coins", tags=["Crypto"])
+async def list_coins(
+    page_num: int = Query(1, ge=1, description="Page number for pagination"),
+    per_page: int = Query(10, ge=1, description="Number of items per page")
+):
+    """
+    List all the avaiable cryptocurrency coins with pagination
+    """
+
+    all_coins = await get_all_coins()
+
+    start_index = (page_num - 1)*per_page
+    end_index = start_index + per_page
+    paginated_coins = all_coins[start_index:end_index]
+    
+    return {
+        "page_num": page_num,
+        "per_page": per_page,
+        "total_coins": len(all_coins),
+        "data": paginated_coins
+    }
+
--- app/services/coingecko.py
+++ app/services/coingecko.py
@@ -20,4 +20,20 @@ async def ping_coingecko()->dict:
         except httpx.RequestError as exc:
             logger.error(f"Failed to reach Coingecko {exc}")
             return {"status":"unreachable", "version":"unknown"}
+
+async def get_all_coins()-> dict:
+    """
+    Fetches the list of all supported coins from CoinGecko
+    """
+
+    async with httpx.AsyncClient() as client:
+        try:
+            # Fetching all coins from CoinGecko
+            response = await client.get(f"{COINGECKO_BASE_URL}/coins/list", timeout=10.0)
+            response.raise_for_status()
+            return response.json()
+
+        except httpx.RequestError as exc:
+            logger.error(f"Error fetching coins {exc}")
+            return []
         
